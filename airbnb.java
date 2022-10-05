30ac36f4f2af548eefc2d9dfd66c782d02468757
shazm12 <shamik.bera2019@vitstudent.ac.in>
2022-10-05 22:56:44 +0530

    Adding 2 files to my project

---
+++ airbnb.java
@@ -0,0 +1,6 @@
+import java.util.*;
+class airbnb {
+    public static void main(String[] args) {
+        System.out.println("Airbnb");
+    }
+}
---
+++ airbnb1.java
@@ -0,0 +1,6 @@
+import java.util.*;
+class airbnb1 {
+    public static void main(String[] args) {
+        System.out.println("Airbnb 1");
+    }
+}
